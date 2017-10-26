088bb401b42ac8d54067a1348da7a6b7b9b9c863
rex5207 <abab5207@gmail.com>
2017-10-26 22:51:36 +0800

    add VCR configure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,17 @@ require 'yaml'
 
 require_relative '../lib/youtube_api.rb'
 
-QUERY_NAME = 'tomato recipe'
+QUERY_NAME = 'tomato recipe'.freeze
 CONFIG = YAML.safe_load(File.read('./config/secrets.yml'))
+TOKEN = CONFIG['GOOGLE_API_KEY']
 CORRECT = YAML.load(File.read('./spec/fixtures/results.yml'))
+
 CASSTTE_FILE = 'youtube_api'.freeze
+CASSETTES_FOLDER = 'spec/fixtures/cassettes'.freeze
+
+VCR.configure do |c|
+  c.cassette_library_dir = CASSETTES_FOLDER
+  c.hook_into :webmock
+  c.filter_sensitive_data('<API_KEY>') { TOKEN }
+  c.filter_sensitive_data('<API_KEY_ESC>') { CGI.escape(TOKEN) }
+end
