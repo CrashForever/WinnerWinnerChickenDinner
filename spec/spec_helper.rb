e4cdba635715ece0b0d702b4bdd92b6159df3fc6
rex5207 <abab5207@gmail.com>
2017-10-27 16:53:10 +0800

    add api test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,21 @@
+ENV['RACK_ENV'] = 'test'
+
 require 'simplecov'
 SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/rg'
 require 'yaml'
+require 'rack/test'
+require 'vcr'
+require 'webmock'
 
-
-require_relative '../lib/youtube_api.rb'
+require_relative 'test_load_all'
 
 QUERY_NAME = 'tomato recipe'.freeze
 CONFIG = YAML.safe_load(File.read('./config/secrets.yml'))
 TOKEN = CONFIG['GOOGLE_API_KEY']
 CORRECT = YAML.load(File.read('./spec/fixtures/results.yml'))
 
-CASSTTE_FILE = 'youtube_api'.freeze
 CASSETTES_FOLDER = 'spec/fixtures/cassettes'.freeze
 
 VCR.configure do |c|
