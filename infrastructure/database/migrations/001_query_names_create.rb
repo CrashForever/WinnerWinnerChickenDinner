fbed77e11c80b176f4714d739f6b488036dda3c3
歐博文 <wen94319@gmail.com>
2017-11-15 23:52:57 +0800

    test with some changes

--- infrastructure/database/migrations/001_query_names_create.rb
+++ infrastructure/database/migrations/001_query_names_create.rb
@@ -6,7 +6,7 @@ Sequel.migration do
   change do
     create_table(:queryNames) do
       primary_key :id
-      String      :query_name
+      String      :query_name, unique: false, null: false
       foreign_key :queryResults_id, :queryResults
 
       DateTime :created_at
--- infrastructure/database/migrations/002_query_results_create.rb
+++ infrastructure/database/migrations/002_query_results_create.rb
@@ -6,8 +6,8 @@ Sequel.migration do
   change do
     create_table(:queryResults) do
       primary_key :id
-      String      :video_id
-      
+      String      :video_id, unique: true, null: false
+
       DateTime :created_at
       DateTime :updated_at
     end
