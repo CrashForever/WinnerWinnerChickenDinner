d93015c920cf1705641e488e54613e8c37458658
歐博文 <wen94319@gmail.com>
2017-12-11 09:24:50 +0800

    Restore video id to db ,but changed db not being UNIQUE, so now it can search same results

--- infrastructure/database/migrations/001_query_results_create.rb
+++ infrastructure/database/migrations/001_query_results_create.rb
@@ -6,7 +6,7 @@ Sequel.migration do
   change do
     create_table(:queryResults) do
       primary_key :id
-      String      :video_id, unique: true, null: false
+      String      :video_id, null: false
 
       DateTime :created_at
       DateTime :updated_at
