55fb4fa7573d9b47cfcf818e71fbd63ebecc1dd9
歐博文 <wen94319@gmail.com>
2018-01-05 10:01:34 +0800

    Add camera photo

--- application/controllers/vision.rb
+++ application/controllers/vision.rb
@@ -32,6 +32,34 @@ module VideosPraise
           end
         end
       end
+      routing.on 'camera_photo' do
+        routing.post do
+          puts "in camera_photo vision.rb"
+          photo = routing.params['img']
+
+          #puts Api.config.methods
+          analyzed_result = CameraPhotoAnalyze.new.call(
+            config: Api.config,
+            photo: photo
+          )
+
+          # http_response = HttpResponseRepresenter
+          #                 .new(Result.new(:ok, 'Analyze success'))
+          # response.status = http_response.http_code
+          # http_response.to_json # if result.success?
+
+
+          http_response = HttpResponseRepresenter.new(analyzed_result.value)
+
+          response.status = http_response.http_code
+          if analyzed_result.success?
+            response['Location'] = "/api/v0.1/vision/upload"
+            ImageAnalyzedRepresenter.new(analyzed_result.value.message).to_json
+          else
+            http_response.to_json
+          end
+        end
+      end
       routing.on 'oauth2callback' do
         routing.get do
           puts 'hi'
